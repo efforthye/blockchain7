0445b3148a9fda9efc67e031259eeca8bf63b220
efforthye <efforthye@gmail.com>
2022-11-29 11:20:18 +0900

    Redux 코드 작성됨

--- New.No.048_20221129/redux-test2/src/components/Count1.jsx
+++ New.No.048_20221129/redux-test2/src/components/Count1.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 
-const Count1Comp = () =>{
-    return <div></div>;
+const Count1Comp = ({ count1, plus, minus, input }) => {
+    const [inputNum, setInputNum] = useState(0);
+    return (
+        <div>
+            <div>{count1}</div>
+            <div>
+                <button onClick={() => { plus() }}>+</button>
+                <button onClick={() => { minus() }}>-</button>
+                <div>
+                    <input value={inputNum} type={'number'} placeholder={"count1"} onInput={(e)=>{
+                        setInputNum(+e.target.value);
+                    }} />
+                    <button onClick={() => { input() }}>=</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Count1Comp;
--- New.No.048_20221129/redux-test2/src/containers/Count1.jsx
+++ New.No.048_20221129/redux-test2/src/containers/Count1.jsx
@@ -1,7 +1,28 @@
 import Count1Comp from '../components/Count1';
 
+import store from '../store';
+import { action } from '../modules/count1';
+import { useState } from 'react';
+
 const Count1Container = () =>{
-    return <Count1Comp/>;
+    const count1 = store.getState().count1;
+
+    const [_, render] = useState(true);
+
+    const plus = () =>{
+        store.dispatch(action.plus);
+        render((state)=> !state);
+    }
+    const minus = () =>{
+        store.dispatch(action.minus);
+        render((state)=> !state);
+    }
+    const input = (input) =>{
+        store.dispatch(action.input(input));
+        render((state)=> !state);
+    }
+
+    return <Count1Comp count1={count1} plus={plus} minus={minus} input={input} />;
 }
 
 export default Count1Container;
