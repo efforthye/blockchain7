82b7209a6cc4cc08e6484030bf5e214aabe1b389
efforthye <efforthye@gmail.com>
2022-11-30 14:00:47 +0900

    로그인 유저 컴포넌트에 띄우기

--- New.No.049_20221130/redux-login/src/components/User/Info/Component.jsx
+++ New.No.049_20221130/redux-login/src/components/User/Info/Component.jsx
@@ -1,4 +1,10 @@
-const InfoComponent = () =>{
-    return <div></div>;
+import styled from "styled-components";
+
+const InfoComponent = ({userName}) =>{
+    return <InfoBox>{userName}님 어서오세요.</InfoBox>;
 }
 export default InfoComponent;
+
+const InfoBox = styled.div`
+    
+`;
--- New.No.049_20221130/redux-login/src/components/User/Info/Container.jsx
+++ New.No.049_20221130/redux-login/src/components/User/Info/Container.jsx
@@ -1,6 +1,16 @@
+import { connect } from "react-redux";
 import InfoComponent from "../Info/Component";
 
-const InfoContainer = () =>{
-    return <InfoComponent/>;
+const InfoContainer = ({userName}) =>{
+    console.log(userName);
+    return <InfoComponent userName={userName} />;
 }
-export default InfoContainer;
+
+const mapStateToProps = (state, props) =>{
+    return {
+        // store에 넣은 것처럼 다시 빼냄
+        userName : state.userInfo.userName,
+    }
+} 
+
+export default connect(mapStateToProps)(InfoContainer);
