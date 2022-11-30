1058cc798b28ea9e3316692c358cfaf0288891d3
efforthye <efforthye@gmail.com>
2022-11-30 14:56:06 +0900

    리둑스 회원가입 로그인 홈 라우터 구현 완료

--- New.No.049_20221130/redux-login/src/components/User/index.jsx
+++ New.No.049_20221130/redux-login/src/components/User/index.jsx
@@ -4,23 +4,45 @@ import RegistContainer from "./Regist/Container";
 import InfoContainer from './Info/Container';
 
 // state의 userName을 보내기 위함
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+
+// 라우터 
+import { Routes, Route, Link } from 'react-router-dom';
 
 // 유저에 대한 총체적인 컴포넌트
 // 유저이름 mapStateToProps에서 받아옴.
-const UserComponent = ({userName}) =>{
+const UserComponent = ({ userName }) => {
     return (
-    <UserBox>
-        {/* 유저 이름이 있으면 유저정보 컨테이너만 띄우고, 없으면 회원가입 로그인을 띄운다. */}
-        {userName ? (<InfoContainer />) : (<><RegistContainer /><LogInContainer /></>)}
-    </UserBox>
+        <UserBox>
+            <div>
+                {/* 홈은 기본적으로 띄우고, 만약 닉네임이 있으면 회원가입과 로그인 안보이게 함 */}
+                <Link to={'/'}>Home</Link>
+                {userName ? <></> : (<>
+                    {" "}
+                    | <Link to={'/regist'}>Regist</Link>
+                    {" "}
+                    | <Link to={'/login'}>LogIn</Link>
+                </>)}
+            </div>
+
+            {/* 유저 이름이 있으면 유저정보 컨테이너만 띄우고, 없으면 회원가입 로그인을 띄운다. */}
+            {/* 이해안됨 */}
+            {userName ? (<InfoContainer />) : <></>}
+
+            {/* 라우트들 자체는 밖에 있든없든 상관없음 */}
+            <Routes>
+                <Route path='/regist' element={<RegistContainer />} ></Route>
+                <Route path='/login' element={<LogInContainer />} ></Route>
+            </Routes>
+            
+        </UserBox>
     );
 }
 
 // state의 userName을 보냄
-const mapStateToProps = (state, props)=>{
+const mapStateToProps = (state, props) => {
     return {
-        userName : state.userInfo.userName
+        userName: state.userInfo.userName
     };
 }
 
