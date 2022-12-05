16ba2dad228e04e2ddaffce154a01680d996a69b
efforthye <efforthye@gmail.com>
2022-12-05 10:25:37 +0900

    프로미스 사용 자세한

--- New.No.052_20221205/redux-thunk/src/modules/promiseTime.js
+++ New.No.052_20221205/redux-thunk/src/modules/promiseTime.js
@@ -49,20 +49,68 @@ console.log("동기 확인");
 // console.log("동기 확인");
 
 // 이처럼 작성하는 경우가 많기 때문에 콜백지옥이라고 부른다.
-const callbackHell = (text, func) =>{
-    setTimeout(()=>{
+const callbackHell = (text, func) => {
+    setTimeout(() => {
         console.log(text);
-        if(func) func();
-    },1000);
+        if (func) func();
+    }, 1000);
 };
 
 // Promise는 위와 같은 콜백 지옥을 피하기 위해 사용한다.
-callbackHell("비동기 확인", ()=>{
-    callbackHell("비동기 확인2", ()=>{
-        callbackHell("비동기 확인3", ()=>{
-            callbackHell("비동기 확인4", ()=>{
+callbackHell("비동기 확인", () => {
+    callbackHell("비동기 확인2", () => {
+        callbackHell("비동기 확인3", () => {
+            callbackHell("비동기 확인4", () => {
                 callbackHell("비동기 확인5");
             });
         });
     });
 });
+
+
+const callbackPromise = (text, time) => {
+    return new Promise((resolve, reject) => {
+        // resolve : 성공 시 결과를 전달한다.
+        // 전달할 데이터 : resolve의 매개변수로 전달해준다.
+        // reject : 실패 시 에러를 전달한다. 사용법은 resolve와 같다.
+        // try 스코프 내의 코드를 실행한다. 도중 에러가 발생하면 catch error로 전달한다.
+
+        try {
+            if(text === "프로미스4"){
+                reject("end");
+            }
+            setTimeout(() => {
+                resolve(text);
+            }, time * 1000);
+        } catch (error) {
+            // 그래서 보통 catch에 reject()를 많이 적는다.
+            reject(error);
+        }
+    });
+}
+
+// .then()은 promise 작업 성공 시 실행하는 콜백함수를 전달한다.
+// 콜백함수의 매개변수는 76번 줄의 resolve에 전달된 매개변수(text를 result로)를 받는다.
+// .catch()는 promise 작업 도중 reject 발생 시 그 것을 받아 처리한다.
+// reject()가 호출되면 then이 아닌 .catch()에서 그 결과를 받는다.
+callbackPromise("프로미스1", 1)
+    .then((result) => {
+        console.log(result);
+        // 다음 프라미스(작업)는 return으로 반환하며, then으로 아래의 결과를 받아올 수 있따.
+        return callbackPromise("프로미스2", 1);
+    })
+    .then((result)=>{
+        console.log(result);
+        return callbackPromise("프로미스3", 1);
+    })
+    .then((result)=>{
+        console.log(result);
+        return callbackPromise("프로미스4", 1);
+    })
+    .then((result)=>{
+        console.log(result);
+        return callbackPromise("프로미스5", 1);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
