78c9a36c174190eac5194e2079a14d5c4bd4674f
efforthye <efforthye@gmail.com>
2022-12-08 02:54:17 +0900

    라우터

--- New.No.034_hw/routes/index.js
+++ New.No.034_hw/routes/index.js
@@ -2,7 +2,7 @@
 
 // const user = require("./user.js");
 
-// // 이놈 무슨 역할 하는 거지? : 
+// // 이놈 무슨 역할 하는 거지? : 미들웨어
 // router.use("/", (req,res,next)=>{
 //     console.log("routes/index.js : "+req.url);
 //     next(); //(중요)
