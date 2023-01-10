1f839e0f262cd260e5a5b84f4c60360766c513f5
efforthye <efforthye@gmail.com>
2023-01-10 12:17:59 +0900

    Jest로 테스트

--- New.No.059_20230110/block/block.js
+++ New.No.059_20230110/block/block.js
@@ -1,4 +1,5 @@
 const merkle = require("merkle");
+const SHA256 = require("crypto-js").SHA256;
 
 class BlockHeader{
     // private으로 값 선언
@@ -9,13 +10,14 @@ class BlockHeader{
     #difficulty;
     #nonce;
 
-    constructor(_data){
+    constructor(_data, _previoueBlock){
         this.#version = "1.0.0";
         // 머클루트 : data가 있으면 머클루트, 없으면 0 repeat
         this.#merkleRoot = _data ? merkle("sha256").sync(_data).root() : "0".repeat(64);
         // Date : 클래스, now() : static으로 정의된 메서드
-        this.#timestamp = Date.now(); 
-        this.#height = 0;
+        // 체인에 추가될 때의 시점으로 블록 생성 시간을 정의하기 위해 setTimestamp() 메서드를 만들었다.
+        this.setTimestamp; // 체인에 추가될 때
+        this.#height = _previoueBlock ? _previoueBlock.height + 1 : 0;
         this.#difficulty = 0;
         this.#nonce = 0;
     }
@@ -40,6 +42,10 @@ class BlockHeader{
         return this.#nonce;
     }
 
+    // 시간을 수정하는 데에 매개변수가 필요 없으므로 일반 메서드로 만들었다.
+    setTimestamp() {
+        this.#timestamp = Date.now();
+    }
 
 
 }
@@ -52,11 +58,12 @@ class Block extends BlockHeader{
     #data
 
     // 생성자
-    constructor(_data){
+    constructor(_data, _previoueBlock){
         // super : 부모 클래스의 constructor를 호출(실행)해 초기값을 가져온다.
-        super(_data); // 부모에게도 받은 data를 보내준다.
-        this.#previousHash = "0".repeat(64);
-        this.#hash = "0".repeat(64);
+        super(_data, _previoueBlock); // 부모에게도 받은 data를 보내준다.
+        this.#previousHash = _previoueBlock ? _previoueBlock.hash : "0".repeat(64);
+        // createHash(this) : 여기서 this는 객체 자체를 매개변수로 보내겠다는 의미이다.
+        this.#hash = _data&&_previoueBlock ? Block.createHash(this) : "0".repeat(64);
         this.#data = _data; // 값을 받아와야 하므로
     }
 
@@ -71,6 +78,41 @@ class Block extends BlockHeader{
         return this.#data;
     }
 
+    // 블록으로 해시를 만드는 static 메서드
+    static createHash(_block){
+
+        // 블록의 정보를 임시로 합칠 변수
+        let tempStr = "";
+
+        // 이 과정이 끝나면 체인을 연결하게 된다.
+        _block.setTimestamp();
+
+        // 블록의 정보를 합친다.
+        tempStr += _block.version;
+        tempStr += _block.merkleRoot;
+        tempStr += _block.timestamp;
+        tempStr += _block.height;
+        tempStr += _block.difficulty;
+        tempStr += _block.nonce;
+        tempStr += _block.previousHash;
+        // hash는 현재 만들고 있는 키라서 추가하지 않는다.
+        // data는 merkleRoot로 합쳐져 있기 때문에 merkleRoot로 대체한다.
+
+        return SHA256(tempStr).toString().toUpperCase();
+
+    }
+
 }
+
+// 제네시스 블록
+const temp = new Block(["a, b, c"]);
+// 블록으로 해시를 만듦
+const blockHash = Block.createHash(temp);
+
+// 0000000000000000000000000000000000000000000000000000000000000000
+console.log(temp.hash); // 0.64 repeat
+// 013614071417193ECE2320253328D7E1B236EA383645C6A28A4F201608C4EC4F
+console.log(blockHash); 
+
 
 module.exports = Block;
---
+++ New.No.059_20230110/block/block.test.js
@@ -0,0 +1,34 @@
+const Block = require("./block.js");
+const merkle = require("merkle");
+
+describe("Block Test", ()=>{
+    // 우리가 만든 블록의 머클루트와 라이브러리로 머클루트 만든 것이 같은지 확인한다.
+    it("merkle Test", () =>{
+        const data = ["a", "b", "c"];
+        const block = new Block(data);
+
+        const merkleRoot = merkle("sha256").sync(data).root();
+        expect(block.merkleRoot).toBe(merkleRoot);
+    
+    });
+
+    // hash를 확인한다.
+    it("hash Test", () =>{
+        const data = ["a", "b", "c"];
+
+        // 이전 블록이 없기 때문에 해시가 0으로 채워졌다.
+        // const block = new Block(data);
+
+        // const hash = Block.createHash(block);
+        // expect(block.hash).toBe(hash);
+
+        // 1번 블록
+        const block1 = new Block(data);
+        // 2번 블록
+        const block2 = new Block(data, block1);
+
+        const hash = Block.createHash(block2);
+        expect(block2.hash).toBe(hash);
+    
+    });
+});
