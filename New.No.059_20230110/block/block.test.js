3fcba0703443bb6a630f9037081349602df669e5
efforthye <efforthye@gmail.com>
2023-01-10 13:52:50 +0900

    Update block.test.js

--- New.No.059_20230110/block/block.test.js
+++ New.No.059_20230110/block/block.test.js
@@ -16,21 +16,14 @@ describe("Block Test", ()=>{
     it("hash Test", () =>{
         const data = ["a", "b", "c"];
 
-        // 이전 블록이 없기 때문에 해시가 0으로 채워졌다.
-        // const block = new Block(data);
-
-        // const hash = Block.createHash(block);
-        // expect(block.hash).toBe(hash);
-
-        // 1번 블록
+        // 그냥 블록으로 비교하면 이전 블록이 없어서 해시가 0으로 채워지기 때문에 블록 2개를 만든다.
         const block1 = new Block(data);
-        // 2번 블록
         const block2 = new Block(data, block1);
+
+        console.log(block2.hash);
 
         const hash = Block.createHash(block2);
         expect(block2.hash).toBe(hash);
-
-        // console.log(block2.hash);
     
     });
 });
