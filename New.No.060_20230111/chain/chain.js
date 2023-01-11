3c30ebd131078d38fdb63726b8c8cbce645c3fc8
efforthye <efforthye@gmail.com>
2023-01-11 11:10:10 +0900

    임시저장

--- New.No.060_20230111/chain/chain.js
+++ New.No.060_20230111/chain/chain.js
@@ -10,7 +10,7 @@ class Chain{
         const genesis = new Block(`제네시스 블록 ${new Date()}`);
 
         console.log(new Date());
-        this.#chain.push(genesis);
+        this.#chain.push(genesis); // 블록넣음..........
     }
 
     // 외부에서 췌인접근시 새로운배열(???) 만들어리턴~~!
@@ -19,10 +19,22 @@ class Chain{
         return [...this.#chain];
     }
 
+    // 에드 블록 ㅇ..ㅇ
+    addBlock(_data){
+        // this.#chain[this.#chain.length-1] : 이전 블록 정보를 가져온다.
+        const newBlock = new Block(_data, this.#chain[this.#chain.length-1]);
+        this.#chain.push(newBlock);
+    }
+    // 체인 123 배열일때 4번블록 추가하면 4번블록은3번블록을알구있어야함 ㅇㅅㅇ 프리뷔어스해쉬~!
+    // 그러면 췌인 chain 기준으로 2번 인덱스의 블롹을 가져와야겠지 ㅇㅇㅇ
+    // 체인의 길이 -1 하면 마지막 인뒉스가 나온다! 그렇기때문에 마지막 인덱스에 해당하는 블록 가져와사용한돠
+
+
 }
 
 const chain = new Chain();
 console.log(chain.chain);
+// 배열에푸쉬 ㅇㅅㅇ
 chain.chain.push({data : "???"});
 console.log(chain.chain);
 
