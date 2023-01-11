382585841a6f294a60f3d6e3cd66401c87580ed4
efforthye <efforthye@gmail.com>
2023-01-11 11:43:06 +0900

    대공사

--- New.No.060_20230111/block/block.js
+++ New.No.060_20230111/block/block.js
@@ -1,7 +1,7 @@
 const merkle = require("merkle");
 const SHA256 = require("crypto-js").SHA256;
 
-class BlockHeader{
+class BlockHeader {
 
     version;
     merkleRoot;
@@ -10,21 +10,21 @@ class BlockHeader{
     difficulty;
     nonce;
 
-    constructor(_data, _previoueBlock){
+    constructor(_data, _previousBlock) {
 
         this.version = "1.0.0";
         const merkleRoot = this.createMerkleRoot(_data);
-        if(merkleRoot.isError){
+        if (merkleRoot.isError) {
             this.merkleRoot = "";
             console.error(merkleRoot.msg);
-        }else{
+        } else {
             this.merkleRoot = merkleRoot.value;
         }
-        this.setTimestamp; 
-        this.height = _previoueBlock ? _previoueBlock.height + 1 : 0;
+        this.setTimestamp;
+        this.height = _previousBlock ? _previousBlock.height + 1 : 0;
         this.difficulty = 0;
         this.nonce = 0;
-        
+
     }
 
     // 블록 현재시간 설정
@@ -34,48 +34,66 @@ class BlockHeader{
     }
 
     // 머클루트 생성
-    createMerkleRoot(_data){
+    createMerkleRoot(_data) {
         // 배열이 아니거나 빈배열
-        if(!Array.isArray(_data) || !_data.length){
-            return {isError : true, msg : "Data가 배열이 아닙니다."}
+        if (!Array.isArray(_data) || !_data.length) {
+            return { isError: true, msg: "Data가 배열이 아닙니다." }
         }
-        return {isError : false, value : merkle("sha256").sync(_data).root()};
+        return { isError: false, value: merkle("sha256").sync(_data).root() };
     }
 
 }
 
-class Block extends BlockHeader{
+class Block extends BlockHeader {
 
     previousHash;
     hash;
     data;
 
-    constructor(_data, _previoueBlock){
-        super(_data, _previoueBlock);
-        this.previousHash = _previoueBlock ? _previoueBlock.hash : "0".repeat(64);
+    constructor(_data, _previousBlock) {
+        super(_data, _previousBlock);
+        this.previousHash = _previousBlock ? _previousBlock.hash : "0".repeat(64);
         // 머클루트(정상적 배열 데이터)가 있으면 해시 생성
-        if(this.merkleRoot){
+        if (this.merkleRoot) {
             this.hash = Block.createHash(this);
-        }else{
+        } else {
             this.hash = "";
         }
-        this.data = _data; 
+        this.data = _data;
     }
 
     // 블록해시 생성
-    static createHash(_block){
+    static createHash(_block) {
         let tempStr = "";
         // _block 객체의 key들을 배열로 변환
         const keys = Object.keys(_block);
-        for(let i = 0; i<keys.length; i++){
-            if(keys[i]=== "hash" || keys[i] === "data"){
+        for (let i = 0; i < keys.length; i++) {
+            if (keys[i] === "hash" || keys[i] === "data") {
                 // continue일시 다음 반복으로 건너뛴다.
-                continue; 
+                continue;
             }
             tempStr += _block[keys[i]];
         }
 
         return SHA256(tempStr).toString().toUpperCase();
+    }
+
+    // 생성된 블록 정상적인지 확인
+    static isValidBlock(_newBlock, _previousBlock) {
+        // 높이 확인
+        if (_newBlock.height !== _previousBlock.height + 1) {
+            return { isError: true, msg: "블록의 높이가 이상합니다.." };
+        }
+        // 이전 해시 확인
+        if (_newBlock.previousHash !== _previousBlock.hash) {
+            return { isError: true, msg: "이전 블록의 해시가 다르다." };
+        }
+        // 해시 확인
+        if (_newBlock.hash !== Block.createHash(_newBlock) ) {
+            return { isError: true, msg: "블록의 해시 생성중 오류 발생" };
+        }
+
+        return {isError : false, value : _newBlock};
     }
 
 }
--- New.No.060_20230111/chain/chain.js
+++ New.No.060_20230111/chain/chain.js
@@ -2,44 +2,78 @@ const Block = require("../block/block.js");
 
 class Chain{
     
-    // 프라이빗으로 외부접근막음~~
     #chain;
 
     constructor(){
+
+        // 블록이 들어갈 배열(블록체인)
         this.#chain = [];
         const genesis = new Block(`제네시스 블록 ${new Date()}`);
 
         console.log(new Date());
-        this.#chain.push(genesis); // 블록넣음..........
+        this.#chain.push(genesis); 
     }
 
-    // 외부에서 췌인접근시 새로운배열(???) 만들어리턴~~!
     get chain(){
-        // 새롭게 안만들면 밖에서 푸쒸한게 들어올수 이뜸 ㅠㅠㅠㅠㅠ 그래서 배열에 넣어뜸 ㅇㅅㅇ!!
         return [...this.#chain];
     }
 
-    // 에드 블록 ㅇ..ㅇ
-    addBlock(_data){
-        // this.#chain[this.#chain.length-1] : 이전 블록 정보를 가져온다.
-        const newBlock = new Block(_data, this.#chain[this.#chain.length-1]);
-        this.#chain.push(newBlock);
+    // 최근 만들어진 블록 가져옴
+    get lastBlock(){
+        return this.#chain[this.#chain.length - 1];
     }
-    // 체인 123 배열일때 4번블록 추가하면 4번블록은3번블록을알구있어야함 ㅇㅅㅇ 프리뷔어스해쉬~!
-    // 그러면 췌인 chain 기준으로 2번 인덱스의 블롹을 가져와야겠지 ㅇㅇㅇ
-    // 체인의 길이 -1 하면 마지막 인뒉스가 나온다! 그렇기때문에 마지막 인덱스에 해당하는 블록 가져와사용한돠
 
+    // 블록 생성
+    createBlock(_data){
+        const newBlock = new Block(_data, this.lastBlock);
+
+
+        // const isValid = Block.isValidBlock(newBlock, this.lastBlock);
+        // if(isValid.isError){
+        //     console.error(isValid.msg);
+        //     return null;
+        // }else{
+        //     this.#chain.push(newBlock);
+        //     return newBlock;
+        // }
+
+
+
+        // this.#chain.push(newBlock);
+
+        return this.add2Chain(newBlock);
+
+        
+    }
+
+    // P2P를 위해 ..(체인 밖에서 블록을 생성해서 그 블록을 체인에 추가하기위함)
+    add2Chain(_newBlock){
+        const isValid = Block.isValidBlock(_newBlock, this.lastBlock);
+        if(isValid.isError){
+            console.error(isValid.msg);
+            return null;
+        }else{
+            this.#chain.push(_newBlock);
+            return _newBlock;
+        }
+    }
 
 }
 
 const chain = new Chain();
-console.log(chain.chain);
-// 배열에푸쉬 ㅇㅅㅇ
-chain.chain.push({data : "???"});
+// console.log(chain);
+// console.log(chain.chain);
+
+// chain.chain.push({data : "???"});
+// console.log(chain.chain);
+
+const block = new Block(["qwer"], chain.lastBlock);
+
+chain.createBlock(["asdf"]);
+chain.createBlock(["asdf2"]);
+chain.createBlock(["asd3"]);
+
+chain.add2Chain(block);
+
 console.log(chain.chain);
 
-// a => 어떤 위치에 [] 이라는 값을 넣었다. 그 어떤 위치의 이름이 a다...
-const a = [];
-const b = a;
-b.push("asdf");
-console.log(a);
